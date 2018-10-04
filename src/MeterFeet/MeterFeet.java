89219a0333be287a295b6208447e9a9ac87f7279
Martin Pedersen <42993919+MartinLieb@users.noreply.github.com>
2018-10-04 14:20:24 +0200

    added text

--- src/MeterFeet/MeterFeet.java
+++ src/MeterFeet/MeterFeet.java
@@ -2,29 +2,29 @@ package MeterFeet;
 
 public class MeterFeet {
 
-    public static double meterToFoot(double meter){
+    public static double meterTofeet(double meter){
 
-        double foot = 0;
-        foot = 3.279 * meter;
-        return foot;
+        double feet = 0;
+        feet = 3.279 * meter;
+        return feet;
 
     }
 
-    public static double footToMeter(double foot){
+    public static double feetToMeter(double feet){
 
         double meter = 0;
-        meter = 0.305 * foot;
+        meter = 0.305 * feet;
         return meter;
 
     }
 
     public static void main(String[] args) {
 
-        double a = footToMeter(10);
-        System.out.println(a);
+        double a = feetToMeter(10);
+        System.out.println("10 feet is = " + a + " meters");
 
-        double b = meterToFoot(10);
-        System.out.println(b);
+        double b = meterTofeet(10);
+        System.out.println("10 meters is = " + b + " feet");
     }
 
 
